8109e9b81cb575ed5089090e0dff5e32ea0cb506
guille <gmoralrubio@gmail.com>
2026-06-03 21:10:08 +0200

    feat: added basic router and layout

--- modules/frontend/src/main.tsx
+++ modules/frontend/src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { RouterProvider } from 'react-router'
+import { router } from '@features/Router/router.tsx'
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 )
---
+++ modules/frontend/src/shared/components/Layout.tsx
@@ -0,0 +1,17 @@
+import { Footer } from '@shared/components/Footer'
+import { Header } from '@shared/components/Header'
+import { Outlet } from 'react-router'
+
+const Layout: React.FC = () => {
+  return (
+    <div className="font-primary container mx-auto max-w-7xl">
+      <Header />
+      <main className="min-h-screen p-6">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default Layout
