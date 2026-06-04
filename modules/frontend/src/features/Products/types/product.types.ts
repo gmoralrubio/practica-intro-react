fc2872ebde7c061f13e81d47084e3c066343ba16
guille <gmoralrubio@gmail.com>
2026-06-04 17:14:40 +0200

    refactor: unified product types and DTOs

---
+++ modules/frontend/src/features/Products/types/product.types.ts
@@ -0,0 +1,40 @@
+export type Tag = 'sport' | 'home' | 'tech' | 'Unknown'
+
+// Domain
+export interface Product {
+	id: number
+	name: string
+	description: string
+	price: number
+	image: string
+	isOnSale: boolean
+	userId: number
+	tags: Tag[]
+	updatedAt: Date
+}
+
+// Se envía a API en creación producto
+export interface CreateProductDTO {
+	name: string
+	description: string
+	price: number
+	image: string
+	isOnSale: boolean
+	tags: string[]
+}
+
+// Se envía a API en edición producto
+export type ProductUpdateDTO = Partial<Product>
+
+// Respuesta API
+export interface ProductResponseDTO {
+	id: number
+	name: string
+	description: string
+	price: number
+	image: string
+	isOnSale: boolean
+	userId: number
+	tags: string[]
+	updatedAt: string
+}
