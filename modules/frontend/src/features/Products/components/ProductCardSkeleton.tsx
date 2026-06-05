c2e9a6f0c00a9c6f90faec51e4eea24cc9b32af8
guille <gmoralrubio@gmail.com>
2026-06-05 17:35:32 +0200

    feat: added product and product list skeleton

---
+++ modules/frontend/src/features/Products/components/ProductCardSkeleton.tsx
@@ -0,0 +1,10 @@
+export const ProductCardSkeleton: React.FC = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="skeleton h-64 w-full"></div>
+      <div className="skeleton h-4 w-28"></div>
+      <div className="skeleton h-4 w-full"></div>
+      <div className="skeleton h-4 w-full"></div>
+    </div>
+  )
+}
