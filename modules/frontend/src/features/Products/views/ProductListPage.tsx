58b79006dd73e0f199a3e957b6a0f50dd8679ae7
guille <gmoralrubio@gmail.com>
2026-06-05 13:58:49 +0200

    feat: added product cards to product list

--- modules/frontend/src/features/Products/views/ProductListPage.tsx
+++ modules/frontend/src/features/Products/views/ProductListPage.tsx
@@ -1,3 +1,17 @@
+import { ProductCard } from '@features/Products/components/ProductCard'
+import { useProducts } from '@features/Products/hooks/useProducts'
+
 export const ProductListPage: React.FC = () => {
-  return <h1>ProductListPage</h1>
+  const { products } = useProducts()
+
+  return (
+    <main className="grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 lg:grid-cols-3">
+      {products?.map((product) => (
+        <ProductCard
+          key={product.id}
+          product={product}
+        />
+      ))}
+    </main>
+  )
 }
