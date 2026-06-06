a8947d6c5e038979f0108575c7fbd9981637dbae
guille <gmoralrubio@gmail.com>
2026-06-06 20:36:05 +0200

    feat: added loading and error state to product list page

--- modules/frontend/src/features/Products/views/ProductListPage.tsx
+++ modules/frontend/src/features/Products/views/ProductListPage.tsx
@@ -1,18 +1,23 @@
 import { ProductCard } from '@features/Products/components/ProductCard'
 import { useProducts } from '@features/Products/hooks/useProducts'
+import { ProductListLoadingPage } from '@features/Products/views/ProductListLoadingPage'
 
 const ProductListPage: React.FC = () => {
-  const { products } = useProducts()
+  const { products, error, isLoading } = useProducts()
 
   return (
-    <main className="grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 lg:grid-cols-3">
-      {products?.map((product) => (
-        <ProductCard
-          key={product.id}
-          product={product}
-        />
-      ))}
-    </main>
+    <>
+      {isLoading && <ProductListLoadingPage />}
+      {error && <p>{error.message}</p>}
+      <main className="grid grid-cols-1 gap-4 p-4 sm:grid-cols-2 lg:grid-cols-3">
+        {products?.map((product) => (
+          <ProductCard
+            key={product.id}
+            product={product}
+          />
+        ))}
+      </main>
+    </>
   )
 }
 export default ProductListPage
