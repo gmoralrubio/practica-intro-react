000137fa57d4616e1727f3d3a274edfd3858d5bd
guille <gmoralrubio@gmail.com>
2026-06-08 20:33:35 +0200

    refactor: simplified logic for skeletons creation

--- modules/frontend/src/features/Products/views/ProductListPage/ProductListLoadingPage.tsx
+++ modules/frontend/src/features/Products/views/ProductListPage/ProductListLoadingPage.tsx
@@ -1,10 +1,9 @@
 import { ProductCardSkeleton } from '@features/Products/components/ProductCardSkeleton'
 
 export const ProductListLoadingPage: React.FC = () => {
-  const loadingElements = []
-  for (let i = 0; i < 20; i++) {
-    loadingElements.push(<ProductCardSkeleton key={i} />)
-  }
+  const loadingElements = Array.from({ length: 20 }, (_, i) => (
+    <ProductCardSkeleton key={i} />
+  ))
   return (
     <main className="grid grid-cols-1 gap-12 p-4 sm:grid-cols-2 lg:grid-cols-3">
       {loadingElements.map((el) => el)}
