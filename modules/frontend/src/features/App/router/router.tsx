c7b104157a6ae30b81fb73e08d98d53437189c18
guille <gmoralrubio@gmail.com>
2026-06-08 20:23:41 +0200

    feat: added auth routes

--- modules/frontend/src/features/App/router/router.tsx
+++ modules/frontend/src/features/App/router/router.tsx
@@ -5,6 +5,8 @@ import { loadProductDetail, loadProducts } from '@features/App/router/loaders'
 import ProductDetailPage from '@features/Products/views/ProductDetailPage/ProductDetailPage'
 import ProductListPage from '@features/Products/views/ProductListPage/ProductListPage'
 import { ProductDetailLoadingPage } from '@features/Products/views/ProductDetailPage/ProductDetailLoadingPage'
+import { LoginPage } from '@features/Auth/views/LoginPage'
+import { RegisterPage } from '@features/Auth/views/RegisterPage'
 
 export const router = createBrowserRouter([
   {
@@ -28,6 +30,13 @@ export const router = createBrowserRouter([
             errorElement: <p>Error al cargar el producto</p>,
             Component: ProductDetailPage,
           },
+        ],
+      },
+      {
+        path: 'auth',
+        children: [
+          { path: 'login', Component: LoginPage },
+          { path: 'register', Component: RegisterPage },
         ],
       },
     ],
