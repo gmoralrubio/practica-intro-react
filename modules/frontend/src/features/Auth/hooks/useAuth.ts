1f9ca117338c19894a1bd84dbcfcae70ed71162d
guille <gmoralrubio@gmail.com>
2026-06-09 19:05:04 +0200

    refater: replaced useContext with use

--- modules/frontend/src/features/Auth/hooks/useAuth.ts
+++ modules/frontend/src/features/Auth/hooks/useAuth.ts
@@ -1,8 +1,8 @@
 import { AuthContext } from '@features/Auth/context/AuthContext'
-import { useContext } from 'react'
+import { use } from 'react'
 
 export function useAuth() {
-	const context = useContext(AuthContext)
+	const context = use(AuthContext)
 	if (!context) {
 		throw new Error('useAuth must be used within AuthProvider')
 	}
