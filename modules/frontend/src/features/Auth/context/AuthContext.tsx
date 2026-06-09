87c81f0285b358c80d90d7593855095489df763a
guille <gmoralrubio@gmail.com>
2026-06-09 16:32:38 +0200

    feat: added authentication context

---
+++ modules/frontend/src/features/Auth/context/AuthContext.tsx
@@ -0,0 +1,124 @@
+import { authRepository } from '@features/Auth/services/authRepository'
+import type { User } from '@features/Auth/types/user.type'
+import { createContext, useContext, useEffect, useState } from 'react'
+
+export interface AuthContextType {
+  user: User | null
+  isAuthenticated: boolean
+  isLoading: boolean
+  error: string | null
+  login: (email: string, password: string) => Promise<void>
+  logout: () => void
+  checkAuth: () => Promise<void>
+}
+
+const AuthContext = createContext<AuthContextType | null>(null)
+
+interface AuthState {
+  user: User | null
+  isLoading: boolean
+  error: string | null
+}
+
+const initialState: AuthState = {
+  user: null,
+  isLoading: false,
+  error: null,
+}
+
+interface AuthProviderProps {
+  children: React.ReactNode
+}
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [state, setState] = useState(initialState)
+
+  useEffect(() => {
+    checkAuth()
+  }, [])
+
+  const login = async (email: string, password: string) => {
+    setState((prev) => ({ ...prev, isLoading: true }))
+    try {
+      const token = await authRepository.loginUser(email, password)
+      localStorage.setItem('accessToken', token.accessToken)
+      const user = await authRepository.getUserInfo(token.accessToken)
+      setState((prev) => ({
+        ...prev,
+        isLoading: false,
+        user,
+      }))
+    } catch (error: unknown) {
+      setState((prev) => ({
+        ...prev,
+        isLoading: false,
+        error: error instanceof Error ? error.message : 'Unknown error',
+      }))
+      throw error
+    }
+  }
+
+  const logout = () => {
+    setState((prev) => ({
+      ...prev,
+      isLoading: true,
+      user: null,
+    }))
+    localStorage.removeItem('accessToken')
+    setState(() => ({
+      error: null,
+      isLoading: false,
+      user: null,
+    }))
+  }
+
+  const checkAuth = async () => {
+    setState((prev) => ({ ...prev, isLoading: true }))
+    try {
+      const token = localStorage.getItem('accessToken') as string
+      if (!token) {
+        setState((prev) => ({
+          ...prev,
+          isLoading: false,
+        }))
+        return
+      }
+      const user = await authRepository.getUserInfo(token)
+      setState((prev) => ({
+        ...prev,
+        isLoading: false,
+        user,
+      }))
+    } catch (error: unknown) {
+      setState((prev) => ({
+        ...prev,
+        error: error instanceof Error ? error.message : 'Unknown error',
+      }))
+      throw error
+    }
+  }
+
+  return (
+    <AuthContext
+      value={{
+        user: state.user,
+        isAuthenticated: state.user !== null,
+        isLoading: state.isLoading,
+        error: state.error,
+        login,
+        logout,
+        checkAuth,
+      }}
+    >
+      {children}
+    </AuthContext>
+  )
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext)
+  if (!context) {
+    throw new Error('useAuth must be used within AuthProvider')
+  }
+  return context
+}
--- modules/frontend/src/main.tsx
+++ modules/frontend/src/main.tsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router'
 import { router } from '@features/App/router/router'
 import './index.css'
+import { AuthProvider } from '@features/Auth/context/AuthContext'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>
 )
