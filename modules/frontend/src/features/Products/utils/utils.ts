fddb4f5762daa47e406ddd89591ebe1717f97b78
guille <gmoralrubio@gmail.com>
2026-06-09 19:06:15 +0200

    refactor: modified format currency

--- modules/frontend/src/features/Products/utils/utils.ts
+++ modules/frontend/src/features/Products/utils/utils.ts
@@ -1,6 +1,8 @@
+const formatCurrency = new Intl.NumberFormat('de-DE', {
+	style: 'currency',
+	currency: 'EUR',
+})
+
 export const toEuro = (num: number): string => {
-	return new Intl.NumberFormat('de-DE', {
-		style: 'currency',
-		currency: 'EUR',
-	}).format(num)
+	return formatCurrency.format(num)
 }
