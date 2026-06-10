0d1045c54751f3e4c2d3d5c4ed6c7e485f8f6fd3
guille <gmoralrubio@gmail.com>
2026-06-10 17:25:34 +0200

    feat: added users product list loading page

--- modules/frontend/src/features/Products/views/ProductListPage/ProductListLoadingPage.tsx
+++ modules/frontend/src/features/Products/views/ProductListPage/ProductListLoadingPage.tsx
@@ -1,12 +1,50 @@
-import { ProductCardSkeleton } from '@features/Products/components/ProductCardSkeleton'
+const SkeletonRow: React.FC<{ index: number }> = ({ index }) => (
+  <tr key={index}>
+    <td>
+      <div className="skeleton h-5 w-32"></div>
+    </td>
+    <td>
+      <div className="skeleton h-5 w-16"></div>
+    </td>
+    <td>
+      <div className="flex gap-1">
+        <div className="skeleton h-5 w-14"></div>
+        <div className="skeleton h-5 w-14"></div>
+      </div>
+    </td>
+    <td>
+      <div className="skeleton h-5 w-24"></div>
+    </td>
+    <td>
+      <div className="flex gap-2">
+        <div className="skeleton h-8 w-16"></div>
+        <div className="skeleton h-8 w-20"></div>
+      </div>
+    </td>
+  </tr>
+)
 
 export const ProductListLoadingPage: React.FC = () => {
   const loadingElements = Array.from({ length: 20 }, (_, i) => (
-    <ProductCardSkeleton key={i} />
+    <SkeletonRow
+      key={i}
+      index={i}
+    />
   ))
   return (
-    <main className="grid grid-cols-1 gap-12 p-4 sm:grid-cols-2 lg:grid-cols-3">
-      {loadingElements.map((el) => el)}
+    <main className="overflow-x-auto p-4">
+      <table className="table table-zebra">
+        <thead>
+          <tr>
+            <th>Nombre</th>
+            <th>Precio</th>
+            <th>Tags</th>
+            <th>Actualizado</th>
+            <th className="w-0"></th>
+          </tr>
+        </thead>
+        <tbody>{loadingElements}</tbody>
+      </table>
     </main>
   )
 }
