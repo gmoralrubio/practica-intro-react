4f7ae3da70e58bb813e6dd00b2cfd01b026dbb29
guille <gmoralrubio@gmail.com>
2026-06-10 17:19:43 +0200

    feat: added format date

--- modules/frontend/src/features/Products/utils/utils.ts
+++ modules/frontend/src/features/Products/utils/utils.ts
@@ -6,3 +6,10 @@ const formatCurrency = new Intl.NumberFormat('de-DE', {
 export const toEuro = (num: number): string => {
 	return formatCurrency.format(num)
 }
+export const formatDate = (date: Date) => {
+	return date.toLocaleDateString('es-ES', {
+		year: 'numeric',
+		month: '2-digit',
+		day: '2-digit',
+	})
+}
