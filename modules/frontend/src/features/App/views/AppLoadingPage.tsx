ec44c4eff07997a8501da1ae4b2befe93439672c
guille <gmoralrubio@gmail.com>
2026-06-10 17:26:04 +0200

    feat: added common product list loading page

---
+++ modules/frontend/src/features/App/views/AppLoadingPage.tsx
@@ -0,0 +1,12 @@
+import { ProductCardSkeleton } from '@features/Products/components/ProductCardSkeleton'
+
+export const AppLoadingPage: React.FC = () => {
+  const loadingElements = Array.from({ length: 20 }, (_, i) => (
+    <ProductCardSkeleton key={i} />
+  ))
+  return (
+    <main className="grid grid-cols-1 gap-12 p-4 sm:grid-cols-2 lg:grid-cols-3">
+      {loadingElements.map((el) => el)}
+    </main>
+  )
+}
